1c43dc846e5a9ae7ffbf8e3a5eca5f15306ee2a0
João Flach <joaoflach@gmail.com>
2023-08-23 13:08:54 -0300

    initial commit with maven project structure

---
+++ src/main/java/com/flach/studies/App.java
@@ -0,0 +1,13 @@
+package com.flach.studies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
